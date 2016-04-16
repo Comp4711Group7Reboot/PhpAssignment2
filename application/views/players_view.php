e4ef9843650f977320a00ec25068c99a8df5f85d
mebighobo <john_chang69@hotmail.com>
2016-04-16 12:03:49 -0700

    added the session and user sql, enhance the layout of the stock and the player view.

--- application/views/players_view.php
+++ application/views/players_view.php
@@ -1,3 +1,12 @@
-{players}
-   <p><a href="player/{Player}">{Player}</a></p>
-{/players}
+<div class="row">
+    <div class="col-md-6">
+        <h1>This is the Player page</h1>
+        <br>
+        {players}
+        <dl>
+            <dt>Name:</dt>
+            <dd><p><a href="player/{Player}">{Player}</a></p></dd>
+        </dl>
+        {/players}
+    </div>
+</div>
