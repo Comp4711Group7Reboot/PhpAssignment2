c18d991c462829f0f01eb551fce0ab9ab299529a
mebighobo <john_chang69@hotmail.com>
2016-04-16 12:07:50 -0700

    added the registration view.

---
+++ application/views/registration.php
@@ -0,0 +1,6 @@
+<form name="register" method="post" action="/auth/register">
+  Username: <input type="text" name="userid"></input><br/>
+  Password: <input type="password" name="password"></input><br/>
+  Password Confrimation: <input type="password" name="passwordconfirmation"></input><br/>
+  <input type="submit">Submit</input>
+</form>
