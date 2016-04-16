b340e02a0f25711152079d33a04e75ad457705e6
mebighobo <john_chang69@hotmail.com>
2016-04-15 20:23:11 -0700

    modified homepage controller,view

--- application/views/player_portfolio.php
+++ application/views/player_portfolio.php
@@ -1,4 +1,20 @@
-{playerprofile}
-    Name: {Player}
-    Cash: {Cash}
-{/playerprofile}
+<div class="row">
+  <div class="col-md-2">Players: </div>
+</div>
+<div class="row">
+    <div class="col-md-6">
+        <img width ="100" height = "100" src ={image} />
+    </div>
+    <div class="col-md-6">
+        {playerprofile}
+        <dl>
+            <dt>Name</dt>
+            <dd>{Player}</dd>
+            
+            <dt>Cash</dt>
+            <dd>{Cash}</dd>
+        </dl>
+        {/playerprofile}
+    </div>
+</div>
+
